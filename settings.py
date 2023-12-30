e7b092da0f9cd953e2ff2fe0f7c36d8670f56911
RudolfVonStroheim <mgus92896@gmail.com>
2023-12-30 18:27:13 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QDialog, QFileDialog
+from PyQt5.QtWidgets import QDialog
 from json import dump, load
 from PyQt5.uic import loadUi
-from os import getcwd
+
 
 class SettingsWindow(QDialog):
     def __init__(self):
@@ -11,15 +11,10 @@ class SettingsWindow(QDialog):
         self.load_data()
         self.ok_btn.clicked.connect(self.submit)
         self.cancel_btn.clicked.connect(self.close)
-        self.change_path_btn.clicked.connect(self.change_dir)
-
-    def change_dir(self):
-        pass
 
     def submit(self):
         self.settings['ip'] = self.ip_line.text
         self.settings['port'] = self.port_line.text()
-        self.settings['dir'] = self.path_lbl.text.rstrip('cwd:/')
         with open('settings.json', 'w') as settings_file:
             dump(self.settings, settings_file)
         self.close()
@@ -27,6 +22,5 @@ class SettingsWindow(QDialog):
     def load_data(self):
         with open('settings.json', 'r') as settings_file:
             self.settings = load(settings_file)
-        self.path_lbl.setText('cwd:/' + self.settings['dir'])
         self.ip_line.setText(self.settings['ip'])
         self.port_line.setText(self.settings['port'])
